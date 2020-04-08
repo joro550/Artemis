279615f98ddad9a0f0feb1f60354d8d52add99a8
Mark Davies <mdavies2@webroot.com>
2020-04-08 10:10:02 +0100

    Adding Subscription controller

---
+++ Artemis/Artemis.Web/Server/Controllers/SubscriptionController.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Artemis.Web.Server.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/subscription")]
+    public class SubscriptionController : ControllerBase
+
+    {
+        private readonly IMediator _mediator;
+
+        public SubscriptionController(IMediator mediator)
+            => _mediator = mediator;
+
+        [HttpPost("location")]
+        public Task CreateSubscriptionBasedOnLocation()
+        {
+            var user = this.User.Identity;
+            return Task.CompletedTask;
+        }
+
+
+        [HttpPost("organization")]
+        public Task CreateSubscriptionToOrganization()
+        {
+            return Task.CompletedTask;
+        }
+
+        [HttpPost("event")]
+        public Task CreateSubscriptionToEvent()
+        {
+            return Task.CompletedTask;
+        }
+
+    }
+}
