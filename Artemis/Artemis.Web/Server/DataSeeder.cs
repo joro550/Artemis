6668ca99a90f5da2867fa6ab909fefcb6dd1598f
joro550 <markdavies550@gmail.com>
2020-04-21 17:00:47 +0100

    Fixing data seeder completely

--- Artemis/Artemis.Web/Server/DataSeeder.cs
+++ Artemis/Artemis.Web/Server/DataSeeder.cs
@@ -1,6 +1,5 @@
 ﻿using Bogus;
 using System;
-using System.Linq;
 using System.Threading.Tasks;
 using Artemis.Web.Server.Data;
 using Artemis.Web.Server.Config;
@@ -20,6 +19,10 @@ namespace Artemis.Web.Server
         private readonly ApplicationDbContext _context;
         private readonly IHostEnvironment _environment;
         private readonly UserManager<ApplicationUser> _userManager;
+        private Faker<EventEntity> _events;
+        private Faker<EventUpdateEntity> _updates;
+        private Faker<MessageTemplateEntity> _template;
+        private Faker<OrganizationEntity> _organizations;
 
         public DataSeeder(ApplicationDbContext context, IHostEnvironment environment, 
             UserManager<ApplicationUser> userManager, IOptions<UserConfig> userConfig)
@@ -28,6 +31,31 @@ namespace Artemis.Web.Server
             _environment = environment;
             _userManager = userManager;
             _userConfig = userConfig.Value;
+
+            _organizations = new Faker<OrganizationEntity>()
+                .RuleFor(entity => entity.Id, faker => faker.IndexFaker++)
+                .RuleFor(entity => entity.IsPublished, faker => true)
+                .RuleFor(entity => entity.Name, faker => faker.Company.CompanyName())
+                .RuleFor(entity => entity.Description, faker => faker.Company.Bs());
+
+            _events = new Faker<EventEntity>()
+                .RuleFor(entity => entity.Id, faker => faker.IndexFaker++)
+                .RuleFor(entity => entity.IsPublished, faker => true)
+                .RuleFor(entity => entity.Name, faker => faker.Lorem.Lines(1))
+                .RuleFor(entity => entity.Description, faker => faker.Lorem.Paragraph())
+                .RuleFor(entity => entity.EventType, faker => faker.Random.Enum<EventType>());
+
+            _updates = new Faker<EventUpdateEntity>()
+                .RuleFor(entity => entity.Id, faker => faker.IndexFaker++)
+                .RuleFor(entity => entity.Title, faker => string.Join(" ", faker.Lorem.Words()))
+                .RuleFor(entity => entity.Message, faker => faker.Lorem.Paragraphs(3));
+
+            _template = new Faker<MessageTemplateEntity>()
+                .RuleFor(entity => entity.Id, faker => faker.IndexFaker++)
+                .RuleFor(entity => entity.IsActive, faker => faker.Random.Bool())
+                .RuleFor(entity => entity.Name, faker => faker.Lorem.Word())
+                .RuleFor(entity => entity.Text, faker => faker.Lorem.Paragraph())
+                .RuleFor(entity => entity.MessageEvent, faker => faker.Random.Enum<MessageEvent>());
         }
 
         public async Task Plant()
@@ -40,33 +68,21 @@ namespace Artemis.Web.Server
             await _userManager.CreateAsync(password: "Ii!62s9cVB&%^hF8",
                 user: new ApplicationUser {Email = "user@email.com", PhoneNumber = _userConfig.PhoneNumber, UserName = "user@email.com", EmailConfirmed = true,PhoneNumberConfirmed = true});
 
-            var user = await _userManager.FindByEmailAsync("user@email.com");
-            
-            var organizations = new Faker<OrganizationEntity>()
-                    .RuleFor(entity => entity.IsPublished, faker => true)
-                    .RuleFor(entity => entity.Name, faker => faker.Company.CompanyName())
-                    .RuleFor(entity => entity.Description, faker => faker.Company.Bs());
-
             const int maxRecords = 51;
+            var user = await _userManager.FindByEmailAsync("user@email.com");
 
             var count = 0;
-            foreach (var entity in organizations.GenerateForever())
+            foreach (var entity in _organizations.GenerateForever())
             {
                 await _context.Organizations.AddAsync(entity);
-                count++;
-                if (count < maxRecords) continue;
-
-                await _context.SaveChangesAsync();
-                break;
-            }
-
-            foreach (var organizationEntity in _context.Organizations.ToList())
-            {
                 await _context.Employees.AddAsync(new EmployeeEntity
-                    {OrganizationId = organizationEntity.Id, UserId = user.Id});
+                    { OrganizationId = entity.Id, UserId = user.Id });
 
-                await CreateEvents(organizationEntity, maxRecords);
-                await CreateTemplates(organizationEntity, maxRecords);
+                await CreateEvents(entity, maxRecords);
+                await CreateTemplates(entity, maxRecords);
+
+                count++;
+                if (count >= maxRecords) break;
             }
 
             await _context.SaveChangesAsync();
@@ -74,73 +90,41 @@ namespace Artemis.Web.Server
 
         private async Task CreateEvents(OrganizationEntity org, int maxRecords)
         {
-            var events = new Faker<EventEntity>()
-                .RuleFor(entity => entity.IsPublished, faker => true)
-                .RuleFor(entity => entity.Name, faker => faker.Lorem.Lines(1))
-                .RuleFor(entity => entity.Description, faker => faker.Lorem.Paragraph())
-                .RuleFor(entity => entity.EventType, faker => faker.Random.Enum<EventType>());
-
             var count = 0;
-
-            foreach (var eventEntity in events.GenerateForever())
+            foreach (var eventEntity in _events.GenerateForever())
             {
                 eventEntity.OrganizationId = org.Id;
                 await _context.Events.AddAsync(eventEntity);
-                
-                count++;
-
-                if (count < maxRecords) continue;
-
-                await _context.SaveChangesAsync();
-                break;
-            }
-
-            foreach (var eventEntity in _context.Events.Where(evt => evt.OrganizationId == org.Id))
-            {
                 await CreateEventUpdates(eventEntity, maxRecords);
+
+                count++;
+                if (count >= maxRecords) break;
             }
         }
 
         private async Task CreateEventUpdates(EventEntity eventEntity, int maxRecords)
         {
-            var updates = new Faker<EventUpdateEntity>()
-                .RuleFor(entity => entity.Title, faker => string.Join(" ", faker.Lorem.Words()))
-                .RuleFor(entity => entity.Message, faker => faker.Lorem.Paragraphs(3));
-
             var count = 0;
-            foreach (var update in updates.GenerateForever())
+            foreach (var update in _updates.GenerateForever())
             {
                 update.EventId = eventEntity.Id;
-
                 await _context.EventUpdate.AddAsync(update);
 
                 count++;
-                if (count < maxRecords) continue;
-
-                await _context.SaveChangesAsync();
-                break;
+                if (count >= maxRecords) break;
             }
         }
 
         private async Task CreateTemplates(OrganizationEntity org, int maxRecords)
         {
-            var template = new Faker<MessageTemplateEntity>()
-                .RuleFor(entity => entity.IsActive, faker => faker.Random.Bool())
-                .RuleFor(entity => entity.Name, faker => faker.Lorem.Word())
-                .RuleFor(entity => entity.Text, faker => faker.Lorem.Paragraph())
-                .RuleFor(entity => entity.MessageEvent, faker => faker.Random.Enum<MessageEvent>());
-
             var count = 0;
-            foreach (var @event in template.GenerateForever())
+            foreach (var @event in _template.GenerateForever())
             {
                 @event.OrganizationId = org.Id;
                 await _context.MessageTemplates.AddAsync(@event);
 
                 count++;
-                if (count < maxRecords) continue;
-                
-                await _context.SaveChangesAsync();
-                break;
+                if (count >= maxRecords) break;
             }
         }
     }
